f20d53300d34e49c87e9a46bf105f0adef8c57f0
Kitisak Chaengsiri <chaengsiri.k@gmail.com>
2017-06-26 09:30:08 +0700

    remove log

--- src/asyncValidate.js
+++ src/asyncValidate.js
@@ -1,13 +1,10 @@
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
 
 const asyncValidate = (values, dispatch, component) => {
-  console.log(values)
   const key = Object.keys(component.values)[0]
   // to solve input values is undefined problem
   if (!values[key]) values[key] = component.values[key]
-  console.log(key, component.values)
   return sleep(1000).then(() => {
-    console.log(values[key])
     // simulate server latency
     if (['john', 'paul', 'george', 'ringo'].includes(values[key])) {
       let error = {}
